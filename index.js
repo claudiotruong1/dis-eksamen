0901cf1daffd5bb39a7f0408da914510bd90e0a4
claudiotruong1 <claudiorasmussen@gmail.com>
2022-12-01 20:00:00 +0100

    Creating a sqlite database

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const session = require('express-session');
 const bodyParser = require('body-parser');
 const path = require('path');
+const sqlite3 = require('sqlite3').verbose();
 const app = express();
 
 app.get('/', (req, res) => {
@@ -11,3 +12,24 @@ app.get('/', (req, res) => {
 app.listen(3000, () => {
     console.log('App listening on port 3000')
 });
+
+// Sqlite ting
+const db = new sqlite3.Database('./db.sqlite');
+
+db.serialize(function() {
+  console.log('creating databases if they don\'t exist');
+  db.run('create table if not exists users (userId integer primary key, username text not null, password text not null)');
+});
+
+// Tilføjer user til db
+const addUserToDatabase = (username, password) => { // opretter database
+    db.run(
+      'insert into users (username, password) values (?, ?)',  // sql statement
+      [username, password], 
+      function(err) {
+        if (err) {
+          console.error(err);
+        }
+      }
+    );
+  }
