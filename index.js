8da24b239462e6dfd8cc5845cf00097990a4c3c7
claudiotruong1 <77775806+claudiotruong1@users.noreply.github.com>
2022-12-01 19:41:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('<h1>My Node App</h1>');
+    res.send('<h1>My Node App claudio</h1>');
 })
 
 app.listen(3000, () => {
