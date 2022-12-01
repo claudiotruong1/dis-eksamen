0119361a83dd873cd118920fc525d3446cdf2be0
claudiotruong1 <claudiorasmussen@gmail.com>
2022-12-01 19:46:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,9 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('<h1>My Node App claudio</h1>');
+    res.send('<h1>My Node App claudio hej med ig</h1>');
 })
 
 app.listen(3000, () => {
     console.log('App listening on port 3000')
 });
-console.log("hej")
-
-
