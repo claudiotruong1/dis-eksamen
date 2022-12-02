f54fa4f7d82a6ff5c11e4b7a7fc7ce92cdbf3233
claudiotruong1 <claudiorasmussen@gmail.com>
2022-12-02 18:53:47 +0100

    Creating login og signup formular

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const session = require('express-session');
 const bodyParser = require('body-parser');
 const path = require('path');
 const sqlite3 = require('sqlite3').verbose();
+const crypto = require('crypto');
 const app = express();
 
+// route for the main site
 app.get('/', (req, res) => {
-    res.send('<h1>My Node App claudio hej med ig</h1>');
+    return res.sendFile(path.join(__dirname, "views/index.html"))
 })
 
 app.listen(3000, () => {
@@ -21,11 +23,85 @@ db.serialize(function() {
   db.run('create table if not exists users (userId integer primary key, username text not null, password text not null)');
 });
 
+// signup route
   app.get("/signup", (req, res) => {
         return res.sendFile(path.join(__dirname, "views/signup.html"))
-  })
+  });
 
+  // login route
   app.get("/login", (req, res) => {
     return res.sendFile(path.join(__dirname, "views/login.html"))
-})
-  
+});
+
+
+const getUserByUsername = (userName) => {
+  // Smart måde at konvertere fra callback til promise:
+  return new Promise((resolve, reject) => {  
+    db.all(
+      'select * from users where userName=(?)', // fecther en bruger
+      [userName], 
+      (err, rows) => {
+        if (err) {
+          console.error(err);
+          return reject(err);
+        }
+        return resolve(rows);
+      }
+    );
+  })
+}
+
+// Tilføjer user til db
+const addUserToDatabase = (username, password) => { // opretter database
+  db.run(
+    'insert into users (username, password) values (?, ?)',  // sql statement
+    [username, password], 
+    function(err) {
+      if (err) {
+        console.error(err);
+      }
+    }
+  );
+}
+const hashPassword = (password) => {
+  const md5sum = crypto.createHash('md5'); // vi bruger md5
+  const salt = 'Some salt for the hash'; // salt = at tilføje støj
+   // funktion som tager et password som er en string
+    return md5sum.update(password + salt).digest('hex'); // lægger salt på password, som er en string før
+  }
+
+
+app.get("/logout", (req, res) => {
+  req.session.destroy((err) => {});
+  return res.send("Thank you! Visit again");
+});
+
+app.get("/signup", (req, res) => {
+  if (req.session.loggedIn) {
+      return res.redirect("/dashboard");
+  } else {
+      return res.sendFile("signup.html", { root: path.join(__dirname, "public") });
+  }
+});
+
+app.post("/signup", bodyParser.urlencoded(), async (req, res) => {
+  const user = await getUserByUsername(req.body.username)
+  if (user.length > 0) {
+    return res.send('Username already exists');
+  }
+
+  let hashPW = hashPassword(req.body.password) // hashing af password
+  addUserToDatabase(req.body.username, hashPW);
+  res.redirect('/login');
+}) 
+
+app.post("/login", bodyParser.urlencoded(), async (req, res) => {
+
+const user = await getUserByUsername (req.body.username)
+  console.log({user});
+
+  if (user.length == 0) { // hvis der ikke er nogen bruger, der er fundet så redircter den
+    console.log("Ingen bruger blev fundet.")
+    return res.redirect("/signup");
+  }
+});
